66e334ce20621201a52cd0ed9795ef76fd20ee65
Kyle Gress <kgress88@gmail.com>
2021-12-21 15:12:42 -0600

    feat!: Move deploys to github actions
    BREAKING CHANGE: changed groupid to io.github.kgress.scaffold

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,27 +1,16 @@
 #!/usr/bin/env bash
-# Update GPG creds
-gpg --allow-secret-key-import --import deploy/seckey.key
-gpg --import deploy/pubkey.key
-# Update the git config with the gpg key
-echo "default-key $GPG_KEYNAME" > $HOME/.gnupg/gpg.conf
-git config --global user.signingkey $GPG_KEYNAME
-# Copy over the settings.xml file
-cp .travis.settings.xml $HOME/.m2/settings.xml
-# Add ssh key
-eval "$(ssh-agent -s)"
-chmod 600 deploy/deploy_rsa
-ssh-add deploy/deploy_rsa
 # Find the most recent version via git tags
-OLD_VERSION=$(npx git-semver-tags | head -n 1)
+PREVIOUS_VERSION=$(npx git-semver-tags | head -n 1);
 # Using conventional commits paradigm, check the merge commit message on master to set up a new version deployment
-if [ -n "$OLD_VERSION" ]; then BUMP_TYPE=$(npx -p conventional-changelog-angular -p conventional-recommended-bump conventional-recommended-bump --preset angular);
-    NEXT_VERSION=$(npx semver -i $BUMP_TYPE ${OLD_VERSION});
+if [ -n "$PREVIOUS_VERSION" ]; then BUMP_TYPE=$(npx -p conventional-changelog-angular -p conventional-recommended-bump conventional-recommended-bump --preset angular);
+    NEXT_VERSION=$(npx semver -i $BUMP_TYPE ${PREVIOUS_VERSION});
     # Add a message to console with the next version number
     echo "Versioning artifacts with version $NEXT_VERSION";
     # Sets the new version for the release and deploys using the maven release profile
-    mvn versions:set --define newVersion=${NEXT_VERSION};
+    mvn -U --no-transfer-progress versions:set --define newVersion=${NEXT_VERSION};
     echo "Performing release for version $NEXT_VERSION";
-    mvn -Prelease -Dtag=${NEXT_VERSION} deploy scm:tag;
+    mvn -U --no-transfer-progress -P release -Dtag=${NEXT_VERSION} deploy scm:tag;
   else
-    echo "Could not detect a previous version with git-semver-tags";
-  fi
+    echo "A problem was encountered when determining the version bump. Please check logs and try again.";
+    return 1;
+fi
